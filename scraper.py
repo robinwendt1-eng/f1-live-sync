f8c50c82e6d731a8e6051ef2a04609da9a5fe0fb
robinwendt1-eng <robinwendt1@gmx.net>
2026-07-05 19:31:20 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,12 +1,30 @@
 import json
+import requests
 from datetime import datetime
 
-data = {
+url = "https://api.jolpi.ca/ergast/f1/current/last/results.json"
+
+response = requests.get(url)
+data = response.json()
+
+race = data["MRData"]["RaceTable"]["Races"][0]
+results = race["Results"]
+
+positions = []
+
+for r in results:
+    positions.append({
+        "position": r["position"],
+        "driver": r["Driver"]["code"]
+    })
+
+output = {
     "timestamp": datetime.utcnow().isoformat(),
-    "status": "ok"
+    "race": race["raceName"],
+    "positions": positions
 }
 
 with open("output.json", "w") as f:
-    json.dump(data, f, indent=2)
+    json.dump(output, f, indent=2)
 
-print("fertig")
+print("F1 Daten geladen")
